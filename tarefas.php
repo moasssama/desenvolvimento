3643e47512c6f45262a1f1a8ced103115158e2c1
moassama <moassama@gmail.com>
2015-09-15 10:20:10 -0300

    4.2 O formulário de cadastro de tarefas pg 32

---
+++ tarefas.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	 <link rel="stylesheet" href="css/style.css">
+	<meta charset="utf-8">
+	<title>Gerenciador de tarefas</title>
+</head>
+<body>
+	<div>
+	</div>
+	<div id="centro"class="forms">
+	<form>
+		<fieldset>
+			<legend>Nova Tarefa</legend>
+				<label>
+					Tarefa:
+					<input type="text" name="nome"/>
+				</label>
+				<input type="submit" name="Cadastrar"/>
+		</fieldset>
+	</form>
+	</div>
+
+</body>
+</html>
