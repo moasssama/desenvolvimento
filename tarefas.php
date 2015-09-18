e2d9c8812d8746b1e00c551e445adf3fddd34c78
moassama <moassama@gmail.com>
2015-09-18 00:31:17 -0300

    dividindo arquivos

--- tarefas.php
+++ tarefas.php
@@ -1,53 +1,41 @@
-﻿<?php session_start()?>
-<!DOCTYPE html>
-<html>
-<head>
-	 <link rel="stylesheet" href="css/style.css">
-	<meta charset="utf-8">
-	<title>Gerenciador de tarefas</title>
-</head>
-<body>
-	<div>
-	</div>
-	<div id="centro"class="forms">
-	<form>
-		<fieldset>
-			<legend>Nova Tarefa</legend>
-				<label>
-					Tarefa:
-					<input type="text" name="nome"/>
-				</label>
-				<input type="submit" value="cadastrar"/>
-		</fieldset>
-	</form>
-	<?php
-		if(isset($_GET['nome'])){
-			$_SESSION['lista_tarefas'][] = $_GET['nome'];
-		} 
-		$lista_tarefas  = array();
-		
-		if(isset($_SESSION['lista_tarefas'])){
-			$lista_tarefas = $_SESSION['lista_tarefas'];
-		}
-	?>
-	</div>
-	<div id="outra" class="forms">
-	<fieldset>
-		<legend>Tarefas</legend>
-		<table>
-			<tr>
-				<th>Tarefas</th>
-			</tr>
-			<?php
-				foreach ($lista_tarefas as $tarefas) :
-			?>
-			<tr>
-				 <td> <?php	echo $tarefas;	?></td>
-			</tr>
-			<?php endforeach?>
-		</table>
-	</fieldset>
-	</div><!---->
-<!---->
-</body>
-</html>
+﻿<?php
+
+session_start();
+	
+if(isset($_GET['nome']) && $_GET['nome']!='' ) {
+	$tarefa=array();
+	
+	$tarefa['nome']=$_GET['nome'];
+	
+	if(isset($_GET['descricao'])) {
+		$tarefa['descricao'] = $_GET['descricao'];
+	} else {
+		$tarefa['descricao'] = '';
+	}
+	
+	if(isset($_GET['prazo'])) {
+		$tarefa['prazo'] = $_GET['prazo'];
+	} else{
+		$tarefa['prazo'] = '';
+	}
+	
+	 $tarefa['prioridade'] = $_GET['prioridade'];
+	
+	if(isset($_GET['concluida'])) {
+		$tarefa['concluida'] = $_GET['concluida'];
+	} else{
+		$tarefa['concluida'] = '';
+	}
+	
+	$_SESSION['lista_tarefas'][] = $tarefa;
+} 
+
+if (isset($_SESSION['lista_tarefas'])) {
+	$lista_tarefas = $_SESSION['lista_tarefas'];
+} else {
+	$lista_tarefas = array();
+}	
+	
+include "template.php";
+?>
+	
