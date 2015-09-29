6f0d81df766fa656965e2f6e88bfe96b1d91d01a
moasssama <moassama@gmail.com>
2015-09-29 01:44:26 -0300

    fim do cap 8

---
+++ cap_08/ajudantes.php
@@ -0,0 +1,44 @@
+<?php
+
+function traduz_concluida($concluida){
+	if($concluida == 1){
+		return 'Sim';
+		}
+		return  'Não';
+		
+}
+function traduz_prioridade($codigo){
+	$prioridade ='';
+	switch ($codigo){
+		case 1:
+			$prioridade = "baixa";
+			break;
+		case 2:
+			$prioridade ="media";
+			break;
+		case 3:
+			$prioridade ="alta";
+			break;
+	}
+	return $prioridade;
+}
+
+function traduz_data_para_banco($data){
+	if ($data==""){
+	return "";
+	}
+	$dados = explode("/",$data);
+	$data_mysql = "{$dados[2]}-{$dados[1]}-{$dados[0]}";
+	return $data_mysql;
+}
+function traduz_data_paraexibir($data){
+	if ($data == "" OR $data == "0000-00-00"){
+		return "";
+	}
+$dados = explode("-",$data);
+$dataexibir = "{$dados[2]}/{$dados[1]}/{$dados[0]}";
+return $dataexibir;
+}
+
+
+?>
---
+++ cap_08/template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <link rel="stylesheet" href="css/style.css">
+	<meta charset="utf-8">
+	<title>Gerenciador de tarefas</title>
+</head>
+<body>
+	
+	<div id="centro"class="forms">
+		<?php include ('formulario.php');?>
+		
+		</div>
+	<div id="outra" class="forms">
+		<?php if ($exibir_tabela) : ?>
+		<?php include ('tabela.php');?>
+		<?php endif;?>
+	</div><!---->
+</body>
+</html>
